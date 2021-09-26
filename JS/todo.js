7d9161b81525ab5c93d9d0c73ea11972c70a856a
maks8686 <36656702+maks8686@users.noreply.github.com>
2021-09-26 13:57:16 +0500

    commit1 поправил форматирование

--- JS/todo.js
+++ JS/todo.js
@@ -1,42 +1,31 @@
+const field = document.querySelector(".field");
+const addBtn = document.querySelector(".add");
+const list = document.querySelector(".list");
 
-const field= document.querySelector('.field');
-const addBtn= document.querySelector('.add');
-const list= document.querySelector('.list');
-console.log('объявили field, add, list');
+function createTask(value) {
+  const task = document.createElement("div");
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  task.textContent = value;
+  task.appendChild(checkBox);
+  checkBox.addEventListener("click", compliteTask);
+  return task;
+}
 
- function createTask(value) {
-    const task = document.createElement('div');
-    const checkBox = document.createElement('input');
-    checkBox.type='checkbox';
-    
-    task.textContent =value;
-    task.appendChild(checkBox);
-    checkBox.addEventListener('click',compliteTask);
-    console.log('createTask прошел');
+function addTaskToList() {
+  if (field.value) {
+    let newTask = createTask(field.value);
+    list.appendChild(newTask);
+  }
+}
 
-    return task;
-    }
-  
- function addTaskToList() {
-     if(field.value){
-         let newTask=createTask(field.value);
-         console.log('addTaskToList прошел');
-         list.appendChild(newTask);
-         
-         
-     }}
- function compliteTask(event) {
-         const target =event.target;
-            parentelement = target.parentElement;
-            if (target.checked== true) {
-                target.parentElement.className = "success" ;
-            }
-            else{
-                target.parentElement.className = '';
-            }
-         }
-         
-     
- 
-addBtn.addEventListener('click', addTaskToList);
- 
+function compliteTask(event) {
+  const target = event.target;
+  if (target.checked) {
+    target.parentElement.className = "success";
+  } else {
+    target.parentElement.className = null;
+  }
+}
+
+addBtn.addEventListener("click", addTaskToList);
